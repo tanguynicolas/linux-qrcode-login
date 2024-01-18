339e73082bb6332143a1e4ae65f6085593120f4e
Tanguy NICOLAS <tanguy-nicolas@outlook.fr>
2024-01-18 21:40:20 +0100

    fix: update rights

--- qrcode-authenticator/etc/nginx/bin/update_pam_uncom.sh
+++ qrcode-authenticator/etc/nginx/bin/update_pam_uncom.sh
@@ -4,5 +4,5 @@ echo "Content-type: text/plain; charset=iso-8859-1"
 echo
 
 # Uncomment QRauthent in PAM config
-sudo sed -i '/^# auth sufficient pam_oauth2_device/ s/# //' /etc/pam.d/lightdm-autologin
+sed -i '/^# auth sufficient pam_oauth2_device/ s/# //' /etc/pam.d/lightdm-autologin
 
